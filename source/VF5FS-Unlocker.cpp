608f00043a8dd16843580509dee4e8df24f8ccc8
Silent <zdanio95@gmail.com>
2021-04-19 18:11:03 +0200

    First commit

---
+++ source/VF5FS-Unlocker.cpp
@@ -0,0 +1,12 @@
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <windows.h>
+
+#include "Utils/MemoryMgr.h"
+#include "Utils/Patterns.h"
+
+void OnInitializeHook()
+{
+
+}
---
+++ source/VersionInfo.lua
@@ -0,0 +1,8 @@
+defines { 
+    "rsc_FullName=\"Virtua Fighter 5 Final Showdown Unlocker\"",
+    "rsc_MinorVersion=0",
+    "rsc_RevisionID=1",
+    "rsc_BuildID=0",
+    "rsc_Copyright=\"2021\"",
+    "rsc_UpdateURL=\"https://github.com/CookiePLMonster/VF5FS-Unlocker/releases\""
+}
